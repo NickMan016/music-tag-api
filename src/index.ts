bb84fa777ac5906a88f7bf01c92e1aa161daa0c5
NickMan016 <npcastillo73@gmail.com>
2023-07-24 19:36:58 -0600

    Cambio de ruta de dominio

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(cors({
-    origin: "https://music-tag.netlify.app/"
+    origin: "https://music-tag.netlify.app"
 }));
 
 app.get('/test', (_, res) => {
